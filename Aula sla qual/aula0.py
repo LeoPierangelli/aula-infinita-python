1c07b3127a265c26c885b839331a22f36d519ed1
Leo Pierangelli <leonardo.pierangelli@gmail.com>
2024-10-16 11:41:38 -0300

    feat bomba atomica

--- Aula sla qual/aula0.py
+++ Aula sla qual/aula0.py
@@ -1,1 +1,38 @@
-print('Hello')
+'''professores = ['Caio', 'Danilo', 'Caio2', 'Luciano', 'André', 'Yan']
+adjetivos = ['1', 'Thor', '2', 'Maromba', 'Maluco', 'Calmo']
+for i in range(len(professores)):
+    print(f'O {professores[i]} é {adjetivos[i]}')
+'''
+'''aux = 0
+valores = [50, 70, 100000, 10, 20]
+maior = valores[0]
+for i in range(len(valores)):
+    if valores[i] > maior:
+        aux = maior
+        maior = valores[i]
+        valores[i] = aux
+print(valores)
+'''
+
+def media_notas(notas):
+    soma = 0
+    for num in notas:
+        soma += num
+    media = soma/len(notas)
+    return media
+
+lista = [3, 8, 5, 9, 10, 9, 7, 2]
+media = media_notas(lista)
+print(media)
+lista_02 = [1,2,3,4,5,6,7]
+media = media_notas(lista_02)
+print(media)
+
+
+
+
+
+
+
+
+
---
+++ Aula sla qual/ex01 lista funcoes.py
@@ -0,0 +1,10 @@
+def somatoria(fodase):
+    soma = 0
+    for num in fodase:
+        soma += num
+    return soma
+
+
+lista_de_numeros = [10,10,10]
+a = somatoria(lista_de_numeros)
+print(a)
